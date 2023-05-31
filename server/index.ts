87477f0fb2535b6759434d57cdb6f386b8c2a7e6
cuongdm <cuongdm1999@gmail.com>
2023-05-31 12:12:12 +0700

    first commit

---
+++ server/index.ts
@@ -0,0 +1,15 @@
+import express = require("express")
+import { Express, Request, Response } from 'express';
+
+const app: Express = express()
+const port = 3000
+
+app.get('/home', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+const sum = (a: number, b: number) => a + b

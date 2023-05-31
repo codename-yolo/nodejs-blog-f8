825bf88c1b54ee78e25db05b2f98d5aa4dfa00fd
cuongdm <cuongdm1999@gmail.com>
2023-05-31 12:25:10 +0700

    config prettier

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,15 @@
-import express = require("express")
+import express = require('express');
 import { Express, Request, Response } from 'express';
 
-const app: Express = express()
-const port = 3000
+const app: Express = express();
+const port = 3000;
 
 app.get('/home', (req: Request, res: Response) => {
-  res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
 
-const sum = (a: number, b: number) => a + b
+const sum = (a: number, b: number) => a + b;
